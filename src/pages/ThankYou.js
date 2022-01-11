cdc813d1f251bd1afc257288bca145a76ca641ae
andurebel <andu.trica@gmail.com>
2022-01-11 23:35:29 +0200

    form done

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -5,15 +5,19 @@ export const ThankYou = () => {
   const { formData } = location.state;
   const { name, message } = formData;
   return (
-    <main className="flex flex-col items-center justify-center p-20">
-      <h1 className="text-2xl ">
-        Thank you for reaching out ,<strong>{name}</strong> !
-      </h1>
+    <main className="flex flex-col items-center justify-center p-20 border border-gray-300 md:container w-3/4 mx-auto my-32 rounded-xl shadow-xl ">
       <h1>
-        {message.length > 0 ? (
+        {message.length > 0 && name.length > 0 ? (
           <div className="text-center mt-8">
+            <h1 className="text-2xl ">
+              Thank you for reaching out,
+              <p>
+                <strong className="text-orange-300 text-center">{name}</strong>{" "}
+                !
+              </p>
+            </h1>
             <h1 className="text-xl py-8">Got below message:</h1>
-            <p className="bg-red-400 rounded p-6">{message}</p>
+            <p className="bg-gray-200 rounded shadow-xl p-6">{message}</p>
           </div>
         ) : (
           <div className="text-center mt-8">
